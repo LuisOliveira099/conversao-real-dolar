af59a743180061f4820ccd17f6db5e720bcd5455
LuisOliveira099 <1592815@sga.pucminas.br>
2026-03-15 13:06:07 -0300

    Add files via upload

---
+++ conversao-real-dolar/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questao_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double reais, dolar;
+            Console.WriteLine("Escreva o valor em reais");
+            reais = double.Parse(Console.ReadLine());
+            dolar = reais / 5.33;
+            Console.WriteLine("Resultado em dolar:" + dolar.ToString("n2"));
+
+
+
+        }
+    }
+}
